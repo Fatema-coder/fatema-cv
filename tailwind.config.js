1288d3dbaffedbc86b4128d077a4e50a83796aad
Fatema Suraj <mdkawser8925@gmail.com>
2025-11-17 12:11:48 +0000

    Initial CV project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.html",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
